9603281927efa65491190b00f37a4877aea06d8f
Emilly Njuguna <emilly.njuguna@smart.local>
2024-04-08 12:22:38 +0300

    Import modules file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { InputComponent } from './components/input/input.component';
+import { MaterialModule } from '../Modules';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
+    
   ],
   providers: [
     provideClientHydration(),
