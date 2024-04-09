3bc10e2a80ae5de8b7acf52d72acde814234c713
Emilly Njuguna <emilly.njuguna@smart.local>
2024-04-09 08:51:32 +0300

    Create route and import modules

--- src/Material.Modules.ts
+++ src/Material.Modules.ts
@@ -1,8 +1,15 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatAutocompleteModule} from "@angular/material/autocomplete"
+import {  MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input"
 
 @NgModule({
     exports:[
-
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule
     ]
 })
 export class MaterailModules{}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutocompleteComponent } from './componenets/autocomplete/autocomplete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'autocomplete', component: AutocompleteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
