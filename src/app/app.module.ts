49888687454a88d814b44a200875cd322670d905
Emilly Njuguna <emilly.njuguna@smart.local>
2024-04-09 08:54:16 +0300

    Import autocomplete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterailModules } from '../Material.Modules';
+import { AutocompleteComponent } from './componenets/autocomplete/autocomplete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutocompleteComponent
   ],
   imports: [
     BrowserModule,
